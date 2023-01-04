3c27ea9045e03b18fec17293099d18b46ccd2638
Namya13Jain <100767035+Namya13Jain@users.noreply.github.com>
2023-01-04 10:06:49 +0530

    Initial Commit

---
+++ Add_First.cpp
@@ -0,0 +1,16 @@
+//Adding an elemnt at the first of the Linked List
+//Basically, inserting an element at the start of the linked list
+struct Node
+{
+    int data;
+    Node *next;
+}*head=nullptr;
+//Adding at the first of the Linked List
+Node *addFirst(Node *temp,int x)
+{
+    
+}
+
+int main(){
+   return 0;
+}
